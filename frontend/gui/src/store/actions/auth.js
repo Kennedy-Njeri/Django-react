1fb376ac96f5411991846a7553fb9dc4cab987f8
Kennedy Njeri <mistakenz123@gmail.com>
2019-10-11 21:26:45 +0300

    more actions types

--- frontend/gui/src/store/actions/auth.js
+++ frontend/gui/src/store/actions/auth.js
@@ -55,6 +55,30 @@ export const authLogin = (username, password) => {
             dispatch(authSuccess(token))
             dispatch(checkAuthTimeout(3600))
 
+        }).catch(err => {
+            dispatch(authFail(err))
+        })
+    }
+}
+
+export const authSignUp = (username, email, password1, password2) => {
+    return dispatch => {
+        dispatch(authStart())
+        axios.post('http://127.0.0.1:8000/rest-auth/registration/', {
+            username: username,
+            email: email,
+            password1: password1,
+            password2: password2
+        }).then(res => {
+            const token = res.data.key
+            const expirationDate = new Date(new Date().getTime() + 3600 * 1000)
+            localStorage.setItem('key', token)
+            localStorage.setItem('expirationDate', expirationDate)
+            dispatch(authSuccess(token))
+            dispatch(checkAuthTimeout(3600))
+
+        }).catch(err => {
+            dispatch(authFail(err))
         })
     }
 }
