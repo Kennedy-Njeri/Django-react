4484d7979fcda1d72527f4c8cb13aba32aedb401
Kennedy Njeri <mistakenz123@gmail.com>
2019-10-12 12:24:18 +0300

    auth, reducers

---
+++ frontend/gui/src/store/reducers/auth.js
@@ -0,0 +1,37 @@
+import * as actionTypes from '../actions/actionsTypes'
+import {updateObject} from '../utility'
+
+const initialState = {
+    token: null,
+    error: null,
+    loading: false
+}
+
+
+const authStart = (state, action) => {
+    return updateObject(state, {
+        error: null,
+        loading: true
+    })
+}
+
+const authSuccess = (state, action) => {
+    return updateObject(state, {
+        token: action.token,
+        error: null,
+        loading: false
+    })
+}
+
+const authFail = (state, action) => {
+    return updateObject(state, {
+        error: action.error,
+        loading: false
+    })
+}
+
+const authLogout = (state, action) => {
+    return updateObject(state, {
+        token: null
+    })
+}
